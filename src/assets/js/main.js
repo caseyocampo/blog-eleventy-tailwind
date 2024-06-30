46391da7914d8c6f40710075f075af2dd43c6247
Casey Ocampo <caseysoflo@gmail.com>
2024-06-30 15:45:32 -0400

    fix: mobile spacing, add new project image, add aria expanded to menu button

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,18 @@
 const menuBtn = document.getElementById("menu-btn");
 const menuList = document.getElementById("menu-list");
+const header = document.getElementsByTagName("header")[0];
 
-menuBtn.addEventListener("click", () => {
-  menuList.classList.toggle("hidden");
-});
+// Responsive navigation menu
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (menuList.classList.contains("hidden")) {
+    menuList.classList.toggle("hidden");
+    menuBtn.setAttribute("aria-expanded", "true");
+    header.classList.add("header-border");
+  } else if (!menuList.classList.contains("hidden")) {
+    menuList.classList.toggle("hidden");
+    menuBtn.setAttribute("aria-expanded", "false");
+    header.classList.remove("header-border");
+  }
+}
