ae839fa2c48b4c3d6fa49f65ba1a493fcc174ef3
Casey Ocampo <caseysoflo@gmail.com>
2024-07-11 18:24:58 -0400

    fix: modify if check

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -29,7 +29,7 @@ let navLinks = document.querySelectorAll(".nav-link");
 for (let i = 0; i < navLinks.length; i++) {
   // Compare the text content of each link with the slug
   if (
-    getSlug(currentUrl)[0] !== ""
+    getSlug(currentUrl) !== null
       ? navLinks[i].textContent.toLowerCase() === getSlug(currentUrl)[0]
       : false
   ) {
