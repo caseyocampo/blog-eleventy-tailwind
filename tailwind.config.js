2ff27dad38813f0d62d215c31163a26c548fc99a
Casey Ocampo <caseysoflo@gmail.com>
2024-07-11 20:01:04 -0400

    feat: add custom breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,liquid}"],
   theme: {
     extend: {
+      screens: {
+        clg: "1125px",
+      },
       spacing: {
         default: "1100px",
       },
